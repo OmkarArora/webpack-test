a148ac957dce8e1e130e68449b08c16ff12fb599
Omkar Arora <omkar@sparklin.com>
2023-08-22 18:27:13 +0530

    feat: added window function example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { sampleFunc } from "./utils";
 
+window.sampleFuncFromWindow = sampleFunc;
+
 document.addEventListener("DOMContentLoaded", () => {
 	const btn = document.getElementById("btn");
 	btn.addEventListener("click", sampleFunc);
